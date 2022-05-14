86d40c2e240ead9c75bf895b3985e2fdc4ff9937
McPixeI <torres.88.bcn@gmail.com>
2022-05-14 18:29:45 +0200

    base styles folder structure & tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      primary: '#6366F1',
+      bgdark: '#111827',
+      bglight: '#fffff',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red
+    },
     extend: {}
   },
   plugins: []
