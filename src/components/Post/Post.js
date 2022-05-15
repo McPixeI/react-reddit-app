20ac253a28348f6ce247bbca7c731843c05b2d2e
McPixeI <torres.88.bcn@gmail.com>
2022-05-15 21:53:05 +0200

    post styling

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,28 +2,48 @@ import { Card } from '../UI/Card'
 import { Image } from '../UI/Image'
 import { Badge } from '../UI/Badge'
 import { CardBody, CardHeading } from '../UI/Card/Card'
-import { ChatAlt2Icon, PhotographIcon, StarIcon } from '@heroicons/react/solid'
+import { ChatAlt2Icon, PhotographIcon, ClockIcon } from '@heroicons/react/outline'
+import { StarIcon } from '@heroicons/react/solid'
 import { relativeDateFormatter, scoreFormatter } from '../../common/utils/helpers/formatters'
 
 export const Post = ({ props }) => {
+  const {
+    thumbnail,
+    author,
+    created,
+    num_comments: numComments,
+    score,
+    title
+  } = props
+
   return (
     <Card className='mb-4'>
       <CardHeading className='aspect-auto overflow-hidden relative'>
-        {props.thumbnail && props.thumbnail !== 'nsfw'
+        {thumbnail && thumbnail !== 'nsfw'
           ? <Image
-              className='object-cover w-full rounded-t-md md:h-auto md:w-48 md:rounded-none md:rounded-l-md'
-              src={props.thumbnail}
+              className='object-cover w-full rounded-t-md md:h-auto md:w-80 md:rounded-none md:rounded-l-md'
+              src={thumbnail}
             />
-          : <PhotographIcon className='w-52 h-52 text-gray-400' />}
+          : <PhotographIcon className='w-full text-gray-400' />}
       </CardHeading>
-
-      <CardBody>
-        {relativeDateFormatter(props.created)}
-        <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>{props.title}</h5>
-        <h2>{props.author}</h2>
-        <div className='flex'><ChatAlt2Icon className='h-4 w-4' />{props.num_comments}</div>
-        {props.score ? <Badge>{scoreFormatter(props.score)}<StarIcon className='w-4 h-4' /></Badge> : null}
-
+      <CardBody className='w-full'>
+        <div className='flex flex-col justify-between mb-2 md:flex-row '>
+          <div className='flex text-sm items-center mb-2 md:m-0'>
+            <h2>Published by {author}</h2>
+          </div>
+          <div className='flex'>
+            <div className='flex mr-4'>
+              <ClockIcon className='h-5 w-5 mt-0.5 mr-0.5' />
+              <time className='mr-4'>
+                {relativeDateFormatter(created)}
+              </time>
+              <ChatAlt2Icon className='h-5 w-5 mt-0.5 mr-0.5' />
+              {numComments}
+            </div>
+            <Badge>{scoreFormatter(score)}<StarIcon className='w-4 h-4 mt-0.5 ml-1' /></Badge>
+          </div>
+        </div>
+        <h3 className='mb-2 max-w-xl text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>{title}</h3>
       </CardBody>
     </Card>
   )
