26c690c685c7c8377b82cc10dd65a27aa1a75a9a
McPixeI <torres.88.bcn@gmail.com>
2022-05-16 11:20:56 +0200

    PostList render refactor

--- src/containers/PostList/PostList.js
+++ src/containers/PostList/PostList.js
@@ -12,52 +12,33 @@ export const PostList = () => {
   const category = useSelector(state => state.categories.value)
   const { data, error, isLoading, isFetching, refetch, startedTimeStamp } = useGetPostsByCategoryQuery(category)
   const [lastUpdate, setLastUpdate] = useState(null)
+  const refreshClasses = clsx('w-5 h-5 mr-1', { 'animate-spin': isFetching || isLoading })
 
   useEffect(() => {
     setLastUpdate(new Date(startedTimeStamp))
   }, [startedTimeStamp])
 
-  /* eslint-disable react/jsx-indent */
-  const ListHeader = () => {
-    const classes = clsx('w-5 h-5 mr-1', { 'animate-spin': isFetching || isLoading })
-    return (
+  if (error) return <p>Sorry, there was an error. Try refreshing the page</p>
+
+  /* eslint-disable react/jsx-closing-tag-location */
+  return (
+    <>
       <div className='flex flex-row items-center mb-4'>
         <Button className='mr-4' onClick={refetch} disabled={isFetching || isLoading}>
-          <RefreshIcon className={classes} />
-          Refresh
+          <RefreshIcon className={refreshClasses} /> Refresh
         </Button>
         {lastUpdate && !isFetching && !isLoading
           ? <div>
             <p className='text-sm font-medium'>/{category}</p>
             <p className='text-sm'>last updated at {dateTimeFormatter(lastUpdate)}</p>
-            </div>
+          </div>
           : null}
       </div>
-    )
-  }
-
-  if (isLoading) return <PostListSkeleton />
-
-  if (isFetching) {
-    return (
-      <>
-        <ListHeader />
-        <PostListSkeleton />
-      </>
-    )
-  }
-
-  if (error) return <p>Sorry, there was an error. Try refreshing the page</p>
-
-  return (
-    <>
-      {data
-        ? (<>
-          <ListHeader />
-          {data.map((post, i) => <Post key={i} props={post.data} />)}
-           </>
-          )
-        : null}
+      {isFetching || isLoading
+        ? <PostListSkeleton />
+        : data
+          ? data.map((post, i) => <Post key={i} props={post.data} />)
+          : null}
     </>
   )
 }
